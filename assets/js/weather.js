a8076e6348a941450361abc6cf70a443f1eb4ac4
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-05-19 17:38:26 +0600

    add background ui part

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,2 +1,33 @@
-const baseUrl = " https://api.weatherapi.com/v1"
-const apiKey = "fea727a39dfa4c9897a54836241905"
+
+const apiResource = {
+    weatherApi: " https://api.weatherapi.com/v1",
+    weatherApiKey: "fea727a39dfa4c9897a54836241905",
+    countryApi: "https://countriesnow.space/api/v0.1/countries"
+}
+const cityList = []
+
+const loadCountryData = async () => {
+    try {
+        const countryList = await fetch(apiResource.countryApi)
+        const { data } = await countryList.json()
+        await data.forEach((country) => country.cities.forEach((city) => cityList.push(city)))
+    } catch (error) {
+        console.log(error)
+    }
+}
+loadCountryData()
+
+const
+
+
+
+
+
+
+
+
+
+
+
+
+
